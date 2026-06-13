5a2e650bc788fca33e32328b7eacec7d78044b4e
freshmozart1 <mail@ole-koester.de>
2026-06-13 13:43:45 +0200

    Refactor: Remove MongoClient and collection exports from database module

--- dist/database/database.js
+++ dist/database/database.js
@@ -1,8 +1,2 @@
-import { MongoClient } from "mongodb";
 export const mongoDbConnectionString = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
-export const client = new MongoClient(mongoDbConnectionString);
-export const database = client.db('jobMatch');
-export const jobsCollection = database.collection('jobs');
-export const coverLettersCollection = database.collection('coverLetters');
-export const cvCollection = database.collection('cv');
 //# sourceMappingURL=database.js.map
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,9 +1,1 @@
-import type { StoredScrapedJob, StoredCoverLetter } from "#types";
-import { MongoClient } from "mongodb";
-
 export const mongoDbConnectionString = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
-export const client = new MongoClient(mongoDbConnectionString);
-export const database = client.db('jobMatch');
-export const jobsCollection = database.collection<StoredScrapedJob>('jobs');
-export const coverLettersCollection = database.collection<StoredCoverLetter>('coverLetters');
-export const cvCollection = database.collection('cv');
